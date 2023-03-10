839633c02c17e70ac6ade00557a4e9eaa6666317
Connor Smith <connor.s1110@gmail.com>
2023-03-10 17:56:46 -0500

    Update RPiC

--- RPiC/RPi_C.py
+++ RPiC/RPi_C.py
@@ -1,13 +1,14 @@
 import paho.mqtt.client as mqtt
 
-broker_IP = 'broker.mqttdashboard.com'
-port = 8000
+broker_IP = ['10.42.0.1', '10.42.0.210']
+port = 1883
 keep_alive = 60
 
 threshold_value = None
+ldr_value = None
 previous_decision = None
 
-topics = [("IoT/lightSensor", 2), ("IoT/threshold", 2)]
+topics = [("lightSensor", 2), ("threshold", 2)]
 
 def on_connect(client, userdata, flags, rc):
     if rc == 0:
@@ -17,14 +18,16 @@ def on_connect(client, userdata, flags, rc):
         print("Did not connect to the Broker with result code " + str(rc))
 
 def on_message(client, userdata, message):
-    global threshold_value, previous_decision
+    global threshold_value, ldr_value, previous_decision
     payload = message.payload.decode("utf-8")
 
     if message.topic == topics[1][0]:
         threshold_value = float(payload)
     else:
-        # Compare LDR value with threshold and generate binary result
         ldr_value = float(payload)
+
+    if threshold_value is not None and ldr_value is not None:
+        # Compare LDR value with threshold and generate binary result
         if ldr_value >= threshold_value:
             result = "TurnOff"
         else:
@@ -32,19 +35,19 @@ def on_message(client, userdata, message):
         # Compare result with previous decision and publish updated decision if changed
         if result != previous_decision:
             previous_decision = result
-            client.publish("IoT/Light_status", result, 2, True)
+            client.publish("LightStatus", result, 2, True)
 
 def run():
     client = mqtt.Client("RPiC", transport = "websockets")
     client.on_connect = on_connect
     client.on_message = on_message
     client.will_set("Status/RaspberryPiC", "offline", 2, True)
-    client.connect(broker_IP, port, keep_alive)
-    client.loop_start()
+    try:
+        client.connect(broker_IP[0], port, keep_alive)
+    except:
+        client.connect(broker_IP[1], port, keep_alive)
+    client.loop_forever()
     client.subscribe(topics)
-
-    while True:
-        continue
 
 if __name__ == "__main__":
     run()
