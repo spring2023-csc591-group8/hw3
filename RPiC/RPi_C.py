1f8fabee52b7ac0d65cc2077176d2ff9b82c9586
ConnorS1110 <connor.s1110@gmail.com>
2023-03-10 18:50:34 -0500

    Removed log messages from RPiC

--- RPiC/RPi_C.py
+++ RPiC/RPi_C.py
@@ -26,7 +26,6 @@ def on_message(client, userdata, message):
     else:
         ldr_value = float(payload)
 
-    print(f"LDR value: {ldr_value}")
     if threshold_value is not None and ldr_value is not None:
         # Compare LDR value with threshold and generate binary result
         if ldr_value >= threshold_value:
@@ -37,7 +36,6 @@ def on_message(client, userdata, message):
         if result != previous_decision:
             previous_decision = result
             client.publish("LightStatus", result, 2, True)
-            print("Published result")
 
 def run():
     client = mqtt.Client("RPiC")
