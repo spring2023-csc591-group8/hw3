574a949d0367dfdcd88555871f68d430064057bb
Stepan Kalinin <skalini@ncsu.edu>
2023-03-11 15:06:43 +0000

    Update interrupt script

--- rpi_a/interrupt_connection.sh
+++ rpi_a/interrupt_connection.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-iptables -I OUTPUT -m owner --gid-owner no-internet -s 0/0 -d 0/0 -j DROP
-iptables -I INPUT -m owner --gid-owner no-internet -s 0/0 -d 0/0 -j DROP
+iptables -I OUTPUT -m tcp -p tcp --dport 1883 -j DROP
+netstat | grep 1883 | awk '{print substr($4, index($4, ":") + 1)}' | xargs -I dest_port iptables -I INPUT -m tcp -p tcp --dport dest_port -j DROP
+iptables -L OUTPUT
+iptables -L INPUT
 read -p "Press [Enter] key to allow internet again"
 iptables -D OUTPUT 1
 iptables -D INPUT 1
